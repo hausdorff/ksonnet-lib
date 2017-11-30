d5d5b0dd986bd42f8877aa81ef1701912e00eda8
bryanl <bryanliles@gmail.com>
2017-11-30 14:08:40 -0500

    removing duplicated code
    Signed-off-by: bryanl <bryanliles@gmail.com>

--- ksonnet-gen/ksonnet/util.go
+++ ksonnet-gen/ksonnet/util.go
@@ -6,36 +6,6 @@ import (
 
 const constructorName = "new"
 
-// isMixinRef will check whether a `ObjectRef` refers to an API object
-// that can be turned into a mixin. This should be true of the vast
-// majority of non-nil `ObjectRef`s. The most common exception is
-// `IntOrString`, which should not be turned into a mixin, and should
-// instead by transformed into a property method that behaves
-// identically to one taking an int or a ref as argument.
-func isMixinRef(or *kubespec.ObjectRef) bool {
-	if or != nil {
-		return false
-	}
-
-	return stringInSlice(string(*or), objectInRefExceptions)
-}
-
-var (
-	objectInRefExceptions = []string{
-		"#/definitions/io.k8s.apimachinery.pkg.util.intstr.IntOrString",
-		"#/definitions/io.k8s.apiextensions-apiserver.pkg.apis.apiextensions.v1beta1.JSONSchemaPropsOrBool",
-	}
-)
-
-func stringInSlice(a string, list []string) bool {
-	for _, b := range list {
-		if b == a {
-			return true
-		}
-	}
-	return false
-}
-
 var (
 	specialProperties = map[kubespec.PropertyName]kubespec.PropertyName{
 		"apiVersion": "apiVersion",
